11ab33d098adc7285c4bdd771f82d3b90e72ad27
RedoTV <94919714+RedoTV@users.noreply.github.com>
2022-04-17 20:42:39 +0300

    Update Program.cs

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -16,10 +16,7 @@ namespace ConsoleApp2
             //    {9, "I" },{ 10 ,"J" },{11 ,"K" },{12 ,"L" },{13 ,"M" },{14 ,"N" }, {15 ,"O" },
             //    {16 ,"P" }, {17 ,"Q" }, {18 ,"R" }, {19 ,"S" }, {20 ,"T" }, {21 ,"U" }, { 22,"V" },
             //    {23 ,"W" }, {24 ,"X" }, {25 ,"Y" }, {26 , "Z" }
-            //};
-            //Console.WriteLine(Object.Equals(alphabet[3],"C"));
-            Console.WriteLine( AlphabetPosition("abc dfgsfsd dhdg"));
-            
+            //};       
         }
         public static string AlphabetPosition(string text)
         {
